8d7123ec70ba685781c402e9dcb613fdac03c439
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-06-22 11:37:33 +0100

    Remove unnecessary print statement

--- lexer.go
+++ lexer.go
@@ -1,13 +1,10 @@
 package expressionparser
 
-import "fmt"
-
 func Tokenize(raw_expression string) []token {
 	runes := []rune(raw_expression)
 	tokens := []token{}
 
 	for i := 0; i < len(runes); {
-		fmt.Printf("Current character: %c\n", runes[i])
 		token, new_index, err := integer_or_error(i, runes)
 		if err == nil {
 			tokens = append(tokens, token)
